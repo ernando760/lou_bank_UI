59cd0b5cf93c38866a65b98b75e8570b60d6ce5d
ErnandoM <ernandomanoel.freire@gmail.com>
2024-02-14 14:58:13 -0300

    adicionando novas rotas

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:lou_bank/src/modules/auth/login/login_in_page.dart';
+import 'package:lou_bank/src/modules/auth/passcode/pass_code_page.dart';
+import 'package:lou_bank/src/modules/splash/splash_page.dart';
 import 'package:lou_bank_core/lou_bank_core.dart';
 
 void main() {
@@ -12,10 +14,13 @@ class LouBankAppMain extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: "/login",
+      initialRoute: "/splash",
+      debugShowCheckedModeBanner: false,
       theme: ThemeMainLouBank.theme,
       routes: {
+        "/splash": (context) => const SplashPage(),
         "/login": (context) => const LoginInPage(),
+        "/pass-code": (context) => const PassCodePage()
       },
     );
   }
