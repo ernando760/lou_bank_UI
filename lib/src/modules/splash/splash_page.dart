11bcc726df0320088576ca9d7d1d907835be7b80
ErnandoM <ernandomanoel.freire@gmail.com>
2024-02-14 14:51:53 -0300

    adicionando uma splash page

---
+++ lib/src/modules/splash/splash_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Navigator.of(context).pushReplacementNamed("/login");
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(''),
+      ),
+      body: Container(),
+    );
+  }
+}
