30c97681f1d3ab254b434287c4e0b815c4f72cc9
ErnandoM <ernandomanoel.freire@gmail.com>
2024-02-22 10:41:45 -0300

    refatorando o codigo

--- lib/src/modules/auth/login/login_in_page.dart
+++ lib/src/modules/auth/login/login_in_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:lou_bank/src/widgets/app_bar_logo.dart';
-import 'package:lou_bank/src/widgets/custom_button_widget.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:lou_bank_core/lou_bank_core.dart';
 
 class LoginInPage extends StatelessWidget {
@@ -8,60 +7,76 @@ class LoginInPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Container(
-          height: MediaQuery.sizeOf(context).height,
-          decoration: const BoxDecoration(
-              color: Colors.amber,
-              gradient: LinearGradient(
-                  begin: Alignment.topRight,
-                  end: Alignment.bottomCenter,
-                  colors: [Color(0xFF433D48), ColorsLouBank.gray1])),
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const AppBarLogo(),
-                const SizedBox(height: 172),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 24),
-                  child: Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        SizedBox(
-                          width: 100,
-                          height: 42,
-                          child: Image.asset(
-                            "assets/images/logo_2.png",
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 84,
-                        ),
-                        CustomButtonWidget(
-                          onPressed: () {
-                            Navigator.of(context)
-                                .pushReplacementNamed("/pass-code");
-                          },
-                          text: "Log in",
-                        ),
-                        const SizedBox(height: 16),
-                        CustomButtonWidget(
-                          isBlack: true,
-                          onPressed: () {},
-                          text: "Become a client of the bank",
-                        )
-                      ],
+    return LouBankScaffold(
+      height: MediaQuery.sizeOf(context).height,
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            LouBankAppBar(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+                title: SvgPicture.asset('assets/images/logo_1.svg'),
+                traling: LouBankCustomIconButton(
+                    icon: const Icon(
+                      Icons.more_vert_outlined,
+                      color: Colors.white,
                     ),
-                  ),
+                    onPressed: () {})),
+            const SizedBox(height: 172),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    SvgPicture.asset(
+                      "assets/images/logo_2.svg",
+                    ),
+                    const SizedBox(
+                      height: 84,
+                    ),
+                    LouBankCustomButton(
+                      onPressed: () {
+                        Navigator.of(context)
+                            .pushReplacementNamed("/pass-code");
+                      },
+                      padding: const EdgeInsets.symmetric(vertical: 13),
+                      decoration: BoxDecoration(
+                          gradient: const LinearGradient(
+                              begin: Alignment.topCenter,
+                              end: Alignment.bottomCenter,
+                              colors: [
+                                Colors.white,
+                                ColorsLouBank.yellow,
+                              ]),
+                          borderRadius: BorderRadius.circular(38)),
+                      width: MediaQuery.sizeOf(context).width,
+                      child: Center(
+                        child: Text(
+                          "Log in",
+                          style: TextStyleLouBank.caption14Medium
+                              .copyWith(fontSize: 15),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    LouBankCustomButton(
+                      padding: const EdgeInsets.symmetric(vertical: 13),
+                      onPressed: () {},
+                      child: Text(
+                        "Become a client of the bank",
+                        style: TextStyleLouBank.caption14Medium
+                            .copyWith(color: Colors.white, fontSize: 15),
+                      ),
+                    )
+                  ],
                 ),
-              ],
+              ),
             ),
-          ),
+          ],
         ),
       ),
     );
--- lib/src/modules/auth/passcode/pass_code_page.dart
+++ lib/src/modules/auth/passcode/pass_code_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:lou_bank/src/widgets/app_bar_logo.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:lou_bank/src/widgets/codes_widget.dart';
-import 'package:lou_bank/src/widgets/custom_bottom_navigation_bar.dart';
 import 'package:lou_bank/src/widgets/keyboard_widget.dart';
 import 'package:lou_bank_core/lou_bank_core.dart';
 
@@ -10,57 +9,89 @@ class PassCodePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: SafeArea(
-          child: Container(
-            alignment: Alignment.center,
-            decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                    begin: Alignment.topRight,
-                    end: Alignment.bottomCenter,
-                    colors: [Color(0xFF433D48), ColorsLouBank.gray1])),
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  const AppBarLogo(),
-                  const SizedBox(height: 68),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 24),
-                    child: Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Text(
-                            "Enter Passcode",
-                            style: TextStyleLouBank.body16Regular
-                                .copyWith(fontSize: 17, color: Colors.white),
-                          ),
-                          const SizedBox(height: 20),
-                          const CodesWidget(),
-                          const SizedBox(height: 59),
-                          const KeyboardWidget(),
-                          const SizedBox(height: 24),
-                          TextButton(
-                            onPressed: () {
-                              Navigator.of(context)
-                                  .pushReplacementNamed("/login");
-                            },
-                            child: Text(
-                              "Can not login?",
-                              style: TextStyleLouBank.caption14Regular.copyWith(
-                                  fontSize: 13, color: ColorsLouBank.yellow),
-                            ),
-                          ),
-                        ],
+    return LouBankScaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              LouBankAppBar(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+                  title: SvgPicture.asset('assets/images/logo_1.svg'),
+                  traling: LouBankCustomIconButton(
+                      icon: const Icon(
+                        Icons.more_vert_outlined,
+                        color: Colors.white,
+                      ),
+                      onPressed: () {})),
+              const SizedBox(height: 68),
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      "Enter Passcode",
+                      style: TextStyleLouBank.body16Regular
+                          .copyWith(fontSize: 17, color: Colors.white),
+                    ),
+                    const SizedBox(height: 20),
+                    const CodesWidget(),
+                    const SizedBox(height: 59),
+                    const KeyboardWidget(),
+                    const SizedBox(height: 24),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pushReplacementNamed("/login");
+                      },
+                      child: Text(
+                        "Can not login?",
+                        style: TextStyleLouBank.caption14Regular.copyWith(
+                            fontSize: 13, color: ColorsLouBank.yellow),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
+            ],
           ),
         ),
-        bottomNavigationBar: const CustomBottomNavigationBar());
+        louBankBottomNavigationBar:
+            LouBankNavigationBar(disableIcon: true, navigationBarButtons: [
+          LouBankNavigationBarItem(
+              icon: SvgPicture.asset("assets/images/home.svg"),
+              activeIcon: SvgPicture.asset(
+                "assets/images/home.svg",
+                colorFilter: const ColorFilter.mode(
+                    ColorsLouBank.yellow, BlendMode.srcIn),
+              )),
+          LouBankNavigationBarItem(
+              icon: SvgPicture.asset("assets/images/shop.svg"),
+              activeIcon: SvgPicture.asset(
+                "assets/images/shop.svg",
+                colorFilter: const ColorFilter.mode(
+                    ColorsLouBank.yellow, BlendMode.srcIn),
+              )),
+          LouBankNavigationBarItem(
+              icon: SvgPicture.asset("assets/images/card.svg"),
+              activeIcon: SvgPicture.asset(
+                "assets/images/card.svg",
+                colorFilter: const ColorFilter.mode(
+                    ColorsLouBank.yellow, BlendMode.srcIn),
+              )),
+          LouBankNavigationBarItem(
+              icon: SvgPicture.asset("assets/images/chat.svg"),
+              activeIcon: SvgPicture.asset(
+                "assets/images/chat.svg",
+                colorFilter: const ColorFilter.mode(
+                    ColorsLouBank.yellow, BlendMode.srcIn),
+              )),
+          LouBankNavigationBarItem(
+              icon: SvgPicture.asset("assets/images/history.svg"),
+              activeIcon: SvgPicture.asset(
+                "assets/images/history.svg",
+                colorFilter: const ColorFilter.mode(
+                    ColorsLouBank.yellow, BlendMode.srcIn),
+              )),
+        ]));
   }
 }
