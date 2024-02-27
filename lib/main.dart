57b3b97806e9b17c2e6665a6968e98d722e91a5b
ErnandoM <ernandomanoel.freire@gmail.com>
2024-02-27 14:44:36 -0300

    news: adicionando novas rotas

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:lou_bank/src/modules/auth/login/login_in_page.dart';
 import 'package:lou_bank/src/modules/auth/passcode/pass_code_page.dart';
 import 'package:lou_bank/src/modules/home/home_page.dart';
+import 'package:lou_bank/src/modules/home/pages/my_card_page.dart';
 import 'package:lou_bank/src/modules/splash/splash_page.dart';
 import 'package:lou_bank_core/lou_bank_core.dart';
 
@@ -22,7 +23,8 @@ class LouBankAppMain extends StatelessWidget {
         "/splash": (context) => const SplashPage(),
         "/login": (context) => const LoginInPage(),
         "/pass-code": (context) => const PassCodePage(),
-        "/home": (context) => const HomePage()
+        "/home": (context) => const HomePage(),
+        "/my-card": (context) => const MyCardPage()
       },
     );
   }
